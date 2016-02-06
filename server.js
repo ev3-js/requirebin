4268cc0dd4595fb55bdbef0035b3cd9ed9345aee
Daniel <danleavitt0@gmail.com>
2016-02-06 08:21:06 -0800

    add server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var app = require('express')()
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + req.path)
 })
 
 app.listen(process.env.PORT)

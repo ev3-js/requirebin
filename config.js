6608a087d8728665b48656d76c07eea829151f1f
Daniel <danleavitt0@gmail.com>
2016-02-06 08:49:29 -0800

    update config

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 var envs = {
   production: {
     GITHUB_CLIENT: '6854b77cb4d0e137ee58',
-    GATEKEEPER: 'https://cycle-shell-gatekeeper.herokuapp.com/',
+    GATEKEEPER: 'https://cycle-shell-gatekeeper.herokuapp.com',
     BROWSERIFYCDN: 'https://wzrd.in'
   },
   dev: {
     GITHUB_CLIENT: '6854b77cb4d0e137ee58', // redirect goes to localhost:5000
-    GATEKEEPER: 'https://cycle-shell-gatekeeper.herokuapp.com/',
+    GATEKEEPER: 'https://cycle-shell-gatekeeper.herokuapp.com',
     BROWSERIFYCDN: 'https://wzrd.in'
   }
 }

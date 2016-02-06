a63d67354f35e40eae62e2d8fe3bbc9059d1bd2d
Daniel <danleavitt0@gmail.com>
2016-02-06 08:48:32 -0800

    update server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var app = require('express')()
+var path = require('path')
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.get('*', function (req, res) {
-  res.sendFile(__dirname + req.path)
+  res.sendFile(path.join(__dirname + req.path))
 })
 
 app.listen(process.env.PORT || 3000)

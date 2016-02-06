56f82db225d533bc7f2906399f010e0bade9182d
Daniel <danleavitt0@gmail.com>
2016-02-06 08:50:36 -0800

    update config

--- embed-bundle-v2.js
+++ embed-bundle-v2.js
@@ -116,7 +116,69 @@ function changeEditor (hash) {
   $('#' + hash + '-link').addClass('btn-primary')
 }
 
-},{"./get-gist-files":2,"iframe":5,"jsonp":4,"url":3}],3:[function(require,module,exports){
+},{"./get-gist-files":2,"iframe":5,"jsonp":4,"url":3}],5:[function(require,module,exports){
+module.exports = function(opts) {
+  return new IFrame(opts)
+}
+
+function IFrame(opts) {
+  if (!opts) opts = {}
+  this.opts = opts
+  this.container = opts.container || document.body
+  this.setHTML(opts)
+}
+
+IFrame.prototype.parseHTMLOptions = function(opts) {
+  if (typeof opts === 'string') opts = {html: opts}
+  if (!opts) opts = {}
+  if (opts.body || opts.head) {
+    if (!opts.body) opts.body = ""
+    if (!opts.head) opts.head = ""
+    opts.html = '<!DOCTYPE html><html><head>' + opts.head + '</head><body>' + opts.body + '</body></html>'
+  }
+  if (!opts.sandboxAttributes) opts.sandboxAttributes = ['allow-scripts']
+  return opts
+}
+
+IFrame.prototype.remove = function() {
+  if (this.iframe) this.container.removeChild(this.iframe)
+}
+
+IFrame.prototype.setHTML = function(opts) {
+  opts = this.parseHTMLOptions(opts)
+  if (!opts.html && !opts.src) return
+  this.remove()
+  
+  // if src is passed in use that (this mode ignores body/head/html options)
+  if (opts.src) {
+    var targetUrl = opts.src
+  } else {
+    // create a blob for opts.html and set as iframe `src` attribute
+    var blob = new Blob([opts.html], { encoding: 'UTF-8', type: 'text/html' })
+    var U = typeof URL !== 'undefined' ? URL : webkitURL
+    var targetUrl = U.createObjectURL(blob)    
+  }
+  // create temporary iframe for generating HTML string
+  // element is inserted into the DOM as a string so that the security policies do not interfere
+  // see: https://gist.github.com/kumavis/8202447
+  var tempIframe = document.createElement('iframe')
+  tempIframe.src = targetUrl
+  tempIframe.setAttribute('scrolling', this.opts.scrollingDisabled ? 'no' : 'yes')
+  tempIframe.style.width = '100%'
+  tempIframe.style.height = '100%'
+  tempIframe.style.border = '0'
+  tempIframe.sandbox = opts.sandboxAttributes.join(' ')
+  if (opts.name) tempIframe.setAttribute('name', opts.name)
+  // generate HTML string
+  var htmlSrc = tempIframe.outerHTML
+  // insert HTML into container
+  this.container.insertAdjacentHTML('beforeend', htmlSrc)
+  // retrieve created iframe from DOM
+  var neighborIframes = this.container.querySelectorAll('iframe')
+  this.iframe = neighborIframes[neighborIframes.length-1]
+}
+
+},{}],3:[function(require,module,exports){
 var punycode = { encode : function (s) { return s } };
 
 exports.parse = urlParse;
@@ -722,69 +784,36 @@ function parseHost(host) {
   return out;
 }
 
-},{"querystring":6}],5:[function(require,module,exports){
-module.exports = function(opts) {
-  return new IFrame(opts)
-}
+},{"querystring":6}],2:[function(require,module,exports){
+var nets = require('nets')
 
-function IFrame(opts) {
-  if (!opts) opts = {}
-  this.opts = opts
-  this.container = opts.container || document.body
-  this.setHTML(opts)
-}
+module.exports = getGistFiles
 
-IFrame.prototype.parseHTMLOptions = function(opts) {
-  if (typeof opts === 'string') opts = {html: opts}
-  if (!opts) opts = {}
-  if (opts.body || opts.head) {
-    if (!opts.body) opts.body = ""
-    if (!opts.head) opts.head = ""
-    opts.html = '<!DOCTYPE html><html><head>' + opts.head + '</head><body>' + opts.body + '</body></html>'
+function getGistFiles (gist, keys, callback) {
+  var files = gist.data.files
+
+  var truncated = keys.filter(function (name) {
+    return files[name] && files[name].truncated
+  })
+
+  if (truncated.length === 0) return callback(null, gist)
+
+  getNext()
+
+  function getNext () {
+    var file = truncated.pop()
+    if (!file) return callback(null, gist)
+    nets({url: files[file].raw_url}, done)
+
+    function done (err, resp, content) {
+      if (err) return callback(err)
+      files[file].content = content
+      setTimeout(getNext)
+    }
   }
-  if (!opts.sandboxAttributes) opts.sandboxAttributes = ['allow-scripts']
-  return opts
 }
 
-IFrame.prototype.remove = function() {
-  if (this.iframe) this.container.removeChild(this.iframe)
-}
-
-IFrame.prototype.setHTML = function(opts) {
-  opts = this.parseHTMLOptions(opts)
-  if (!opts.html && !opts.src) return
-  this.remove()
-  
-  // if src is passed in use that (this mode ignores body/head/html options)
-  if (opts.src) {
-    var targetUrl = opts.src
-  } else {
-    // create a blob for opts.html and set as iframe `src` attribute
-    var blob = new Blob([opts.html], { encoding: 'UTF-8', type: 'text/html' })
-    var U = typeof URL !== 'undefined' ? URL : webkitURL
-    var targetUrl = U.createObjectURL(blob)    
-  }
-  // create temporary iframe for generating HTML string
-  // element is inserted into the DOM as a string so that the security policies do not interfere
-  // see: https://gist.github.com/kumavis/8202447
-  var tempIframe = document.createElement('iframe')
-  tempIframe.src = targetUrl
-  tempIframe.setAttribute('scrolling', this.opts.scrollingDisabled ? 'no' : 'yes')
-  tempIframe.style.width = '100%'
-  tempIframe.style.height = '100%'
-  tempIframe.style.border = '0'
-  tempIframe.sandbox = opts.sandboxAttributes.join(' ')
-  if (opts.name) tempIframe.setAttribute('name', opts.name)
-  // generate HTML string
-  var htmlSrc = tempIframe.outerHTML
-  // insert HTML into container
-  this.container.insertAdjacentHTML('beforeend', htmlSrc)
-  // retrieve created iframe from DOM
-  var neighborIframes = this.container.querySelectorAll('iframe')
-  this.iframe = neighborIframes[neighborIframes.length-1]
-}
-
-},{}],6:[function(require,module,exports){
+},{"nets":7}],6:[function(require,module,exports){
 
 /**
  * Object#toString() ref for stringify().
@@ -1103,36 +1132,7 @@ function decode(str) {
   }
 }
 
-},{}],2:[function(require,module,exports){
-var nets = require('nets')
-
-module.exports = getGistFiles
-
-function getGistFiles (gist, keys, callback) {
-  var files = gist.data.files
-
-  var truncated = keys.filter(function (name) {
-    return files[name] && files[name].truncated
-  })
-
-  if (truncated.length === 0) return callback(null, gist)
-
-  getNext()
-
-  function getNext () {
-    var file = truncated.pop()
-    if (!file) return callback(null, gist)
-    nets({url: files[file].raw_url}, done)
-
-    function done (err, resp, content) {
-      if (err) return callback(err)
-      files[file].content = content
-      setTimeout(getNext)
-    }
-  }
-}
-
-},{"nets":7}],4:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 
 /**
  * Module dependencies
@@ -1396,7 +1396,186 @@ module.exports = Nets
 function Nets(uri, opts, cb) {
   req(uri, opts, cb)
 }
-},{"request":10}],9:[function(require,module,exports){
+},{"request":10}],10:[function(require,module,exports){
+var window = require("global/window")
+var once = require("once")
+var parseHeaders = require('parse-headers')
+
+var messages = {
+    "0": "Internal XMLHttpRequest Error",
+    "4": "4xx Client Error",
+    "5": "5xx Server Error"
+}
+
+var XHR = window.XMLHttpRequest || noop
+var XDR = "withCredentials" in (new XHR()) ? XHR : window.XDomainRequest
+
+module.exports = createXHR
+
+function createXHR(options, callback) {
+    if (typeof options === "string") {
+        options = { uri: options }
+    }
+
+    options = options || {}
+    callback = once(callback)
+
+    var xhr = options.xhr || null
+
+    if (!xhr) {
+        if (options.cors || options.useXDR) {
+            xhr = new XDR()
+        }else{
+            xhr = new XHR()
+        }
+    }
+
+    var uri = xhr.url = options.uri || options.url
+    var method = xhr.method = options.method || "GET"
+    var body = options.body || options.data
+    var headers = xhr.headers = options.headers || {}
+    var sync = !!options.sync
+    var isJson = false
+    var key
+    var load = options.response ? loadResponse : loadXhr
+
+    if ("json" in options) {
+        isJson = true
+        headers["Accept"] = "application/json"
+        if (method !== "GET" && method !== "HEAD") {
+            headers["Content-Type"] = "application/json"
+            body = JSON.stringify(options.json)
+        }
+    }
+
+    xhr.onreadystatechange = readystatechange
+    xhr.onload = load
+    xhr.onerror = error
+    // IE9 must have onprogress be set to a unique function.
+    xhr.onprogress = function () {
+        // IE must die
+    }
+    // hate IE
+    xhr.ontimeout = noop
+    xhr.open(method, uri, !sync)
+                                    //backward compatibility
+    if (options.withCredentials || (options.cors && options.withCredentials !== false)) {
+        xhr.withCredentials = true
+    }
+
+    // Cannot set timeout with sync request
+    if (!sync) {
+        xhr.timeout = "timeout" in options ? options.timeout : 5000
+    }
+
+    if (xhr.setRequestHeader) {
+        for(key in headers){
+            if(headers.hasOwnProperty(key)){
+                xhr.setRequestHeader(key, headers[key])
+            }
+        }
+    } else if (options.headers) {
+        throw new Error("Headers cannot be set on an XDomainRequest object")
+    }
+
+    if ("responseType" in options) {
+        xhr.responseType = options.responseType
+    }
+    
+    if ("beforeSend" in options && 
+        typeof options.beforeSend === "function"
+    ) {
+        options.beforeSend(xhr)
+    }
+
+    xhr.send(body)
+
+    return xhr
+
+    function readystatechange() {
+        if (xhr.readyState === 4) {
+            load()
+        }
+    }
+
+    function getBody() {
+        // Chrome with requestType=blob throws errors arround when even testing access to responseText
+        var body = null
+
+        if (xhr.response) {
+            body = xhr.response
+        } else if (xhr.responseType === 'text' || !xhr.responseType) {
+            body = xhr.responseText || xhr.responseXML
+        }
+
+        if (isJson) {
+            try {
+                body = JSON.parse(body)
+            } catch (e) {}
+        }
+
+        return body
+    }
+
+    function getStatusCode() {
+        return xhr.status === 1223 ? 204 : xhr.status
+    }
+
+    // if we're getting a none-ok statusCode, build & return an error
+    function errorFromStatusCode(status, body) {
+        var error = null
+        if (status === 0 || (status >= 400 && status < 600)) {
+            var message = (typeof body === "string" ? body : false) ||
+                messages[String(status).charAt(0)]
+            error = new Error(message)
+            error.statusCode = status
+        }
+
+        return error
+    }
+
+    // will load the data & process the response in a special response object
+    function loadResponse() {
+        var status = getStatusCode()
+        var body = getBody()
+        var error = errorFromStatusCode(status, body)
+        var response = {
+            body: body,
+            statusCode: status,
+            statusText: xhr.statusText,
+            raw: xhr
+        }
+        if(xhr.getAllResponseHeaders){ //remember xhr can in fact be XDR for CORS in IE
+            response.headers = parseHeaders(xhr.getAllResponseHeaders())
+        } else {
+            response.headers = {}
+        }
+
+        callback(error, response, response.body)
+    }
+
+    // will load the data and add some response properties to the source xhr
+    // and then respond with that
+    function loadXhr() {
+        var status = getStatusCode()
+        var error = errorFromStatusCode(status)
+
+        xhr.status = xhr.statusCode = status
+        xhr.body = getBody()
+        xhr.headers = parseHeaders(xhr.getAllResponseHeaders())
+
+        callback(error, xhr, xhr.body)
+    }
+
+    function error(evt) {
+        callback(evt, xhr)
+    }
+}
+
+
+function noop() {}
+
+},{"global/window":11,"once":12,"parse-headers":13}],9:[function(require,module,exports){
 
 /**
  * This is the common logic for both the Node.js and web browser
@@ -1595,186 +1774,7 @@ function coerce(val) {
   return val;
 }
 
-},{"ms":11}],10:[function(require,module,exports){
-var window = require("global/window")
-var once = require("once")
-var parseHeaders = require('parse-headers')
-
-var messages = {
-    "0": "Internal XMLHttpRequest Error",
-    "4": "4xx Client Error",
-    "5": "5xx Server Error"
-}
-
-var XHR = window.XMLHttpRequest || noop
-var XDR = "withCredentials" in (new XHR()) ? XHR : window.XDomainRequest
-
-module.exports = createXHR
-
-function createXHR(options, callback) {
-    if (typeof options === "string") {
-        options = { uri: options }
-    }
-
-    options = options || {}
-    callback = once(callback)
-
-    var xhr = options.xhr || null
-
-    if (!xhr) {
-        if (options.cors || options.useXDR) {
-            xhr = new XDR()
-        }else{
-            xhr = new XHR()
-        }
-    }
-
-    var uri = xhr.url = options.uri || options.url
-    var method = xhr.method = options.method || "GET"
-    var body = options.body || options.data
-    var headers = xhr.headers = options.headers || {}
-    var sync = !!options.sync
-    var isJson = false
-    var key
-    var load = options.response ? loadResponse : loadXhr
-
-    if ("json" in options) {
-        isJson = true
-        headers["Accept"] = "application/json"
-        if (method !== "GET" && method !== "HEAD") {
-            headers["Content-Type"] = "application/json"
-            body = JSON.stringify(options.json)
-        }
-    }
-
-    xhr.onreadystatechange = readystatechange
-    xhr.onload = load
-    xhr.onerror = error
-    // IE9 must have onprogress be set to a unique function.
-    xhr.onprogress = function () {
-        // IE must die
-    }
-    // hate IE
-    xhr.ontimeout = noop
-    xhr.open(method, uri, !sync)
-                                    //backward compatibility
-    if (options.withCredentials || (options.cors && options.withCredentials !== false)) {
-        xhr.withCredentials = true
-    }
-
-    // Cannot set timeout with sync request
-    if (!sync) {
-        xhr.timeout = "timeout" in options ? options.timeout : 5000
-    }
-
-    if (xhr.setRequestHeader) {
-        for(key in headers){
-            if(headers.hasOwnProperty(key)){
-                xhr.setRequestHeader(key, headers[key])
-            }
-        }
-    } else if (options.headers) {
-        throw new Error("Headers cannot be set on an XDomainRequest object")
-    }
-
-    if ("responseType" in options) {
-        xhr.responseType = options.responseType
-    }
-    
-    if ("beforeSend" in options && 
-        typeof options.beforeSend === "function"
-    ) {
-        options.beforeSend(xhr)
-    }
-
-    xhr.send(body)
-
-    return xhr
-
-    function readystatechange() {
-        if (xhr.readyState === 4) {
-            load()
-        }
-    }
-
-    function getBody() {
-        // Chrome with requestType=blob throws errors arround when even testing access to responseText
-        var body = null
-
-        if (xhr.response) {
-            body = xhr.response
-        } else if (xhr.responseType === 'text' || !xhr.responseType) {
-            body = xhr.responseText || xhr.responseXML
-        }
-
-        if (isJson) {
-            try {
-                body = JSON.parse(body)
-            } catch (e) {}
-        }
-
-        return body
-    }
-
-    function getStatusCode() {
-        return xhr.status === 1223 ? 204 : xhr.status
-    }
-
-    // if we're getting a none-ok statusCode, build & return an error
-    function errorFromStatusCode(status, body) {
-        var error = null
-        if (status === 0 || (status >= 400 && status < 600)) {
-            var message = (typeof body === "string" ? body : false) ||
-                messages[String(status).charAt(0)]
-            error = new Error(message)
-            error.statusCode = status
-        }
-
-        return error
-    }
-
-    // will load the data & process the response in a special response object
-    function loadResponse() {
-        var status = getStatusCode()
-        var body = getBody()
-        var error = errorFromStatusCode(status, body)
-        var response = {
-            body: body,
-            statusCode: status,
-            statusText: xhr.statusText,
-            raw: xhr
-        }
-        if(xhr.getAllResponseHeaders){ //remember xhr can in fact be XDR for CORS in IE
-            response.headers = parseHeaders(xhr.getAllResponseHeaders())
-        } else {
-            response.headers = {}
-        }
-
-        callback(error, response, response.body)
-    }
-
-    // will load the data and add some response properties to the source xhr
-    // and then respond with that
-    function loadXhr() {
-        var status = getStatusCode()
-        var error = errorFromStatusCode(status)
-
-        xhr.status = xhr.statusCode = status
-        xhr.body = getBody()
-        xhr.headers = parseHeaders(xhr.getAllResponseHeaders())
-
-        callback(error, xhr, xhr.body)
-    }
-
-    function error(evt) {
-        callback(evt, xhr)
-    }
-}
-
-
-function noop() {}
-
-},{"global/window":12,"once":13,"parse-headers":14}],12:[function(require,module,exports){
+},{"ms":14}],11:[function(require,module,exports){
 var global=self;if (typeof window !== "undefined") {
     module.exports = window;
 } else if (typeof global !== "undefined") {
@@ -1785,7 +1785,28 @@ var global=self;if (typeof window !== "undefined") {
     module.exports = {};
 }
 
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
+module.exports = once
+
+once.proto = once(function () {
+  Object.defineProperty(Function.prototype, 'once', {
+    value: function () {
+      return once(this)
+    },
+    configurable: true
+  })
+})
+
+function once (fn) {
+  var called = false
+  return function () {
+    if (called) return
+    called = true
+    return fn.apply(this, arguments)
+  }
+}
+
+},{}],14:[function(require,module,exports){
 /**
  * Helpers.
  */
@@ -1913,27 +1934,6 @@ function plural(ms, n, name) {
 }
 
 },{}],13:[function(require,module,exports){
-module.exports = once
-
-once.proto = once(function () {
-  Object.defineProperty(Function.prototype, 'once', {
-    value: function () {
-      return once(this)
-    },
-    configurable: true
-  })
-})
-
-function once (fn) {
-  var called = false
-  return function () {
-    if (called) return
-    called = true
-    return fn.apply(this, arguments)
-  }
-}
-
-},{}],14:[function(require,module,exports){
 var trim = require('trim')
   , forEach = require('for-each')
   , isArray = function(arg) {

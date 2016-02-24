e7c6a3f8be40845be2704e617f858b590ac90c56
Daniel <danleavitt0@gmail.com>
2016-02-23 19:14:52 -0800

    move sanbox actions

--- index.js
+++ index.js
@@ -127,29 +127,6 @@ function initialize () {
       if (sandbox.iframeHead) gist.files['page-head.html'] = {'content': sandbox.iframeHead}
       if (sandbox.iframeBody) gist.files['page-body.html'] = {'content': sandbox.iframeBody}
 
-      // sandbox actions
-      sandbox.on('modules', function (modules) {
-        if (!modules) return
-        packagejson.dependencies = {}
-        modules.forEach(function (mod) {
-          if (mod.core) return
-          packagejson.dependencies[mod.name] = mod.version
-        })
-      })
-
-      sandbox.on('bundleStart', function () {
-        ui.$spinner.removeClass('hidden')
-      })
-
-      sandbox.on('bundleEnd', function (bundle) {
-        ui.$spinner.addClass('hidden')
-      })
-
-      sandbox.on('bundleError', function (err) {
-        ui.$spinner.addClass('hidden')
-        ui.tooltipMessage('error', 'Bundling error: \n\n' + err)
-      })
-
       githubGist.save(gist, id, opts, function (err, newGist) {
         var newGistId = newGist.id
         if (newGist.owner && newGist.owner.login) {
@@ -374,6 +351,30 @@ function initialize () {
     // loads the current code on load
     setTimeout(function () {
       actions.play()
+      // sandbox actions
     }, 500)
+  })
+  setTimeout(function () {
+    sandbox.on('modules', function (modules) {
+      if (!modules) return
+      packagejson.dependencies = {}
+      modules.forEach(function (mod) {
+        if (mod.core) return
+        packagejson.dependencies[mod.name] = mod.version
+      })
+    })
+
+    sandbox.on('bundleStart', function () {
+      ui.$spinner.removeClass('hidden')
+    })
+
+    sandbox.on('bundleEnd', function (bundle) {
+      ui.$spinner.addClass('hidden')
+    })
+
+    sandbox.on('bundleError', function (err) {
+      ui.$spinner.addClass('hidden')
+      ui.tooltipMessage('error', 'Bundling error: \n\n' + err)
+    })
   })
 }

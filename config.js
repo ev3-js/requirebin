d1f0e8ce76ea8400dded16854fac2cf2e7b04cfc
Daniel <danleavitt0@gmail.com>
2016-03-31 18:38:18 -0700

    remove /

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 var envs = {
   production: {
     GITHUB_CLIENT: '4d35ae56b0a8aad5cdbf',
-    GATEKEEPER: 'https://ev3-sh-gatekeeper.herokuapp.com/',
+    GATEKEEPER: 'https://ev3-sh-gatekeeper.herokuapp.com',
     BROWSERIFYCDN: 'https://wzrd.in'
   },
   dev: {
     GITHUB_CLIENT: '4d35ae56b0a8aad5cdbf', // redirect goes to localhost:5000
-    GATEKEEPER: 'https://ev3-sh-gatekeeper.herokuapp.com/',
+    GATEKEEPER: 'https://ev3-sh-gatekeeper.herokuapp.com',
     BROWSERIFYCDN: 'https://wzrd.in'
   }
 }

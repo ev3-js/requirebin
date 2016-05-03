0cbbd0128c8076b617f6a6dd03972f81cbd75fb9
Daniel <danleavitt0@gmail.com>
2016-05-03 15:32:36 -0700

    update robot-loop

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function initialize () {
     'version': '1.0.0',
     'dependencies': {
       'iframe-console': '0.1.13',
-      'robot-loop': '0.1.20'
+      'robot-loop': '0.1.21'
     }
   }
   var parsedURL = url.parse(window.location.href, true)

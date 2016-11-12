95fe5cbede1e51079e19a5fd5582386309fdd2e6
Daniel <danleavitt0@gmail.com>
2016-11-12 13:05:27 -0800

    build

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function initialize () {
     'version': '1.0.0',
     'dependencies': {
       'iframe-console': '0.1.13',
-      'robot-loop': '0.1.39'
+      'robot-loop': '0.1.41'
     }
   }
 
